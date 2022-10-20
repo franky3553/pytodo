6bf891925f5fed4a6b1f9e4e632f745ea3c13871
franky3553 <franky3553@gmail.com>
2022-10-20 04:26:31 +0200

    Def home function
    "Todo" will be a class that we will create later
    "base.html" will be our template that we will create later

--- app.py
+++ app.py
@@ -7,3 +7,11 @@ from flask import Flask, render_template, request, redirect, url_for
 ## /2/ Create app instance 
 
 app = Flask(__name__)
+
+
+## /3/ Def home function 
+
+@app.get("/")
+def home():
+	todo_list = db.session.query(Todo).all()
+	return render_template("base.html", todo_list=todo_list)
