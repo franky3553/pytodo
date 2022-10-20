0dc0396f4e40ed6b45ed2ad81d8086f05d3c7312
franky3553 <franky3553@gmail.com>
2022-10-20 04:32:44 +0200

    Create todo model db
    With three columns: id, title and complete

--- app.py
+++ app.py
@@ -16,10 +16,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app) ## "db" now is "SQLAlchemy(app)"
 
-
 ## /3/ Def home function 
 
 @app.get("/")
 def home():
 	todo_list = db.session.query(Todo).all()
 	return render_template("base.html", todo_list=todo_list)
+
+## /6/ Create todo model db
+
+class Todo(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	title = db.Column(db.String(100))
+	complete = db.Column(db.Boolean)
